243f0003cb7268fbdce454dc1ea87ebafdbdccd4
Andrea <foxus20@gmail.com>
2022-10-12 13:47:22 +0200

    Refactor: removed unuseful comments

--- src/sensors/sensors.service.ts
+++ src/sensors/sensors.service.ts
@@ -1,25 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 import { Sensor } from './entities/sensor.entity';
 import { SensorsRepository } from './sensors.repository';
 
 @Injectable()
 export class SensorsService {
     constructor(
-            //@InjectRepository(Sensor) private sensorsRepository: Repository<Sensor>
             private sensorsRepository: SensorsRepository
         ){}
 
     createOrUpdateSensors(sensors: Sensor[]){
-        // sensors = this.sensorsRepository.create(sensors)
-        // console.log(sensors)
+
         return this.sensorsRepository.save(sensors);
-        //return this.sensorsRepository.upsertCustom(sensors);
-        
-        // return this.sensorsRepository.upsert(sensors, {
-        //     conflictPaths: ["id"],
-        //     skipUpdateIfNoValuesChanged: false, // supported by postgres, skips update if it would not change row values
-        // });
     }
 }
