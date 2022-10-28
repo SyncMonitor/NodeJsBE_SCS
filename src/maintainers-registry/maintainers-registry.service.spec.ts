8ecda53667c73f1940fc1d206182628231c64c50
Andrea <foxus20@gmail.com>
2022-10-28 17:30:26 +0200

    Moved business logic from controller to service for maintainers registry

--- src/maintainers-registry/maintainers-registry.service.spec.ts
+++ src/maintainers-registry/maintainers-registry.service.spec.ts
@@ -1,6 +1,7 @@
 import { createMock } from '@golevelup/ts-jest';
 import { Test, TestingModule } from '@nestjs/testing';
 import { DeleteError } from 'src/exceptions/delete.exception';
+import { InsertError } from 'src/exceptions/insert.exception';
 import { NotFoundError } from 'src/exceptions/not-found.exception';
 import { UpdateError } from 'src/exceptions/update.exception';
 import { MaintainersRegistryRepository } from './maintainers-registry.repository';
@@ -10,6 +11,9 @@ describe('MaintainersRegistryService', () => {
   let maintainersRegistryService: MaintainersRegistryService;
   let maintainersRegistryRepository: MaintainersRegistryRepository;
   let maintainerRegistry;
+  let conflictDatabaseError;
+  let recordInsertedResponse;
+  let recordNotInsertedResponse;
   let recordUpdatedResponse;
   let recordNotUpdatedResponse;
   let recordDeletedResponse;
@@ -33,19 +37,80 @@ describe('MaintainersRegistryService', () => {
   });
 
   maintainerRegistry = {
-        'id': '1',
-        'name': 'Mario',
-        'surname': 'Rossi',
-        'company': 'Sensor repair snc',
-        'phone': '3313333333',
-        'email': 'mario@sensorrepairing.it'
-    }
+      'id': '1',
+      'name': 'Mario',
+      'surname': 'Rossi',
+      'company': 'Sensor repair snc',
+      'phone': '3313333333',
+      'email': 'mario@sensorrepairing.it'
+  };
+
+  conflictDatabaseError = {
+    message: 'conflict database error',
+    code: 23505,
+  };
+
+  recordInsertedResponse = {
+    identifiers: [ { id: '1' } ],
+    generatedMaps: [ { id: '1' } ],
+    raw: [ { id: '1' } ],
+  };
+
+  recordNotInsertedResponse = {
+    identifiers: [ ],
+    generatedMaps: [ ],
+    raw: [ ],
+  };
 
   recordUpdatedResponse = { generatedMaps: [], raw: [], affected: 1 };
   recordNotUpdatedResponse = { generatedMaps: [], raw: [], affected: 0 };
 
   recordDeletedResponse = { raw: [], affected: 1 };
   recordNotDeletedResponse = { raw: [], affected: 0 };
+
+  describe('getMaintainerById', () => {
+    it('should return the maintainer if found', async () => {
+      jest.spyOn(maintainersRegistryRepository, 'findOne')
+        .mockImplementation(() => Promise.resolve(maintainerRegistry));
+
+      const response = maintainersRegistryService.getMaintainerById('1');
+
+      await expect(response).resolves.toEqual(maintainerRegistry);
+    });
+
+    it('should thrown a NotFoundError if the maintainer was not found', async () => {
+      jest.spyOn(maintainersRegistryRepository, 'findOne')
+        .mockImplementation(() => Promise.resolve(null));
+
+      const response = maintainersRegistryService.getMaintainerById('1');
+
+      await expect(response).rejects.toThrow(NotFoundError);
+    });
+  });
+
+  describe('createMaintainer', () => {
+    it('should return the maintainer created if a maintainer was created', async () => {
+      jest.spyOn(maintainersRegistryRepository, 'insert')
+        .mockImplementation(() => Promise.resolve(recordInsertedResponse));
+      jest.spyOn(maintainersRegistryService, 'getMaintainerById')
+        .mockImplementation(() => Promise.resolve(maintainerRegistry));
+
+      const response = 
+        maintainersRegistryService.createMaintainer(maintainerRegistry);
+      
+      await expect(response).resolves.toEqual(maintainerRegistry);
+    })
+
+    it('should thrown an InsertError if it couldn\'t create the record', async () => {
+      jest.spyOn(maintainersRegistryRepository, 'insert')
+        .mockImplementation(() => Promise.resolve(recordNotInsertedResponse));
+
+      const response = 
+        maintainersRegistryService.createMaintainer(maintainerRegistry);
+      
+      await expect(response).rejects.toThrow(InsertError);
+    })
+  })
 
   describe('editMaintainerById', () => {
     it('should return the maintainer registry updated if it was updated', async () => {
@@ -58,16 +123,6 @@ describe('MaintainersRegistryService', () => {
             maintainersRegistryService.editMaintainerById('1', maintainerRegistry);
 
         await expect(response).resolves.toEqual(maintainerRegistry);
-    });
-
-    it('should throw a NotFoundError if the maintainer registry id wasn\'t found', async () => {
-        jest.spyOn(maintainersRegistryService, 'getMaintainerById')
-            .mockImplementation(() => Promise.resolve(null));
-
-        const response = 
-            maintainersRegistryService.editMaintainerById('1', maintainerRegistry);
-
-        await expect(response).rejects.toThrow(NotFoundError);
     });
 
     it('should throw an UpdateError if it couldn\'t update the record', async () => {
@@ -94,16 +149,6 @@ describe('MaintainersRegistryService', () => {
         maintainersRegistryService.deleteMaintainerById('1');
 
       await expect(response).resolves.toBeUndefined();
-    });
-
-    it('should throw a NotFoundError if the maintainer wasn\'t found', async () => {
-      jest.spyOn(maintainersRegistryService, 'getMaintainerById')
-        .mockImplementation(() => Promise.resolve(null));
-
-      const response = 
-        maintainersRegistryService.deleteMaintainerById('1');
-
-      await expect(response).rejects.toThrow(NotFoundError);
     });
 
     it('should throw a DeleteError if it couldn\'t delete the record', async () => {
--- src/maintainers-registry/maintainers-registry.service.ts
+++ src/maintainers-registry/maintainers-registry.service.ts
@@ -5,6 +5,7 @@ import { isEmpty } from 'underscore';
 import { NotFoundError } from 'src/exceptions/not-found.exception';
 import { UpdateError } from 'src/exceptions/update.exception';
 import { DeleteError } from 'src/exceptions/delete.exception';
+import { InsertError } from 'src/exceptions/insert.exception';
 
 @Injectable()
 export class MaintainersRegistryService {
@@ -15,25 +16,38 @@ export class MaintainersRegistryService {
         return this.maintainersRegistryRepository.find();
     }
 
-    getMaintainerById(id: string){
-        return this.maintainersRegistryRepository.findOne({
-            where: {
-                id: id
-            }
-        })
+    async getMaintainerById(id: string){
+        const maintainer = 
+            await this.maintainersRegistryRepository.findOne({
+                where: {
+                    id: id
+                }
+            });
+
+        if(isEmpty(maintainer))
+            throw new NotFoundError('maintainer id not found');
+
+        return maintainer;
     }
 
     async createMaintainer(maintainer: MaintainerRegistry){
-        const response = 
+        const insertResponse = 
             await this.maintainersRegistryRepository.insert(maintainer);
-        const maintainerInsertedId = response.identifiers[0].id;
+
+        if(isEmpty(insertResponse.identifiers))
+            throw new InsertError('problem to insert record');
+
+        const maintainerInsertedId = insertResponse.identifiers[0].id;
 
         return this.getMaintainerById(maintainerInsertedId);
     }
 
     async editMaintainerById(id: string, maintainerRegistry: MaintainerRegistry){
-        if(isEmpty(await this.getMaintainerById(id)))
-            throw new NotFoundError('maintainer id not found');
+        try{
+            await this.getMaintainerById(id);    
+        }catch(error){
+            throw(error);
+        }
         
         const updateResponse = 
             await this.maintainersRegistryRepository.update(id, maintainerRegistry);
@@ -47,8 +61,11 @@ export class MaintainersRegistryService {
     }
 
     async deleteMaintainerById(id: string){
-        if(isEmpty(await this.getMaintainerById(id)))
-            throw new NotFoundError('maintainer id not found');
+        try{
+            await this.getMaintainerById(id);    
+        }catch(error){
+            throw(error);
+        }
 
         const deleteResponse = 
             await this.maintainersRegistryRepository.delete(id);
